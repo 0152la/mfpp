3dd829ced703f6b2a49a756cad42094514a1f736
Andrei Lascu <andrei.lascu10@imperial.ac.uk>
2021-02-05 12:43:31 +0000

    Coverage script improvements

--- scripts/run_and_log.sh
+++ scripts/run_and_log.sh
@@ -12,8 +12,18 @@ time=$5
 
 export GCOV_PREFIX=$gcov_prefix
 
+# Execute experiments
 curr_dir=`pwd`
 cd $spec_ast_dir
 ./scripts/run_experiments.py --append-id --mode time --mode-val $time $spec_ast_yaml
+
+# Gather gcno files
+cd $lib_build_dir
+gcno_archive_name=$(mktemp -u lib_gcno_XXX.tar)
+find . -name "*gcno" -exec tar -rf $gcno_archive_name {} \;
+
+# Move gcno files and gather coverage
 cd $gcov_prefix.$lib_build_dir
+mv $lib_build_dir/$gcno_archive_name .
+tar -xf $gcno_archive_name
 gcovr --object-directory $lib_build_dir
